92f1c8d7fe35a2875b597b8fcb7b5bf3818e57ba
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-06-24 12:26:34 +0800

    AWS Media

--- config/settings/production.py
+++ config/settings/production.py
@@ -59,6 +59,17 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
+INSTALLED_APPS += ['storages']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_AUTO_CREATE_BUCKET = True
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+DEFAULT_FILE_STORAGE = 'steeply.storage_backends.MediaStorage'
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
